6f211ce67adc322d95760bdb8789ef572ccfd1dc
mohsindev369 <mohsinakrambukhari@gmail.com>
2025-04-21 02:10:27 +0500

    drizzle stric true verbose true

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,6 +7,8 @@ export default defineConfig({
     schema: './src/supabase/migrations/schema.ts',
     out: './src/supabase/migrations',
     dialect: 'postgresql',
+    strict:true,
+    verbose:true,
     dbCredentials: {
         url: process.env.DATABASE_URL!,
     },
